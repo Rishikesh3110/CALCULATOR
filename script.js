2832c790c15bba4e8fa92c9362a814b9df19dd5c
Rishikesh3110 <rishikeshganta3110@gmail.com>
2025-02-09 13:56:21 +0530

    script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-// Initialize the display
+
 let currentInput = '';
 
-// Function to append numbers to the display
+
 function appendNumber(number) {
     currentInput += number;
     document.getElementById('display').value = currentInput;
 }
 
-// Function to append operators
 function appendOperator(operator) {
     currentInput += ` ${operator} `;
     document.getElementById('display').value = currentInput;
 }
 
-// Function to clear the display
+
 function clearDisplay() {
     currentInput = '';
     document.getElementById('display').value = '';
 }
 
-// Function to calculate the result
+
 function calculateResult() {
     try {
         let result = eval(currentInput);
